755295bf8152f3675ae283cb7ea0a1bcac4c9217
wanwan <2504034557@qq.com>
2021-12-04 11:10:44 +0800

    修改样式和新增js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const indexBtn = document.querySelector("#indexBtn");
+indexBtn.onclick = function(){
+    alert("index");
+}
+const testBtn = document.querySelector("#testBtn");
+testBtn.onclick = function(){
+    alert("test");
+}
+
